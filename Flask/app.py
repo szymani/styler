797b0dc8380b0dd5b558262caaacdd620869ebd8
Szymani <Szymanski996@gmail.com>
2020-04-21 02:33:16 +0100

    Endpoints for pre made styles and custom styles

--- Flask/app.py
+++ Flask/app.py
@@ -11,10 +11,12 @@ from io import BytesIO
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+style_paths = {'to_day': 'today.jpg', 'to_night': 'night.jpg'}
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = basedir + '/static/images/'
+app.config['STYLE_FOLDER'] = basedir + '/static/styles/'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -30,7 +32,7 @@ class Image(db.Model):
   style_image = db.Column(db.LargeBinary)
   result_image = db.Column(db.LargeBinary)
   password = db.Column(db.Integer)
-  style_type = db.Column(db.Integer)
+  style_type = db.Column(db.String(200))
   status = db.Column(db.Integer)
 
   def __init__(self, content_image, style_type, name='Default name', password='123123', style_image=None):
@@ -69,31 +71,47 @@ class Compute(Thread):
 
     image = Image.query.get(self.data.id)
     image.status = 1
-    image.result_image = image.content_image
+    image.result_image = image.style_image
     app.logger.info("{Id: " +  str(self.data.id) + " --> Status " +  str(image.status) + "}")
     db.session.commit()
 
 
 @app.route('/image/custom', methods=['POST'])
 def add_custom():
-  if request.form['name'] is not None: name = request.form['name']
-
+  #* Check imput
   if request.files['content_image'] is None: return "Missing content image", 200
+  if request.files['style_image'] is None: return "Missing style image", 200
 
   content_image = request.files['content_image']
-  if content_image and allowed_file(content_image.filename):
-    filename = secure_filename(content_image.filename)
-
-    new_image = Image(content_image=content_image.read(), style_type=0, name=name)
-
-    compute_thread = Compute(new_image)
+  style_image = request.files['style_image']
+  if content_image and style_image and allowed_file(content_image.filename) and allowed_file(style_image.filename):
+    new_image = Image(content_image=content_image.read(), style_image=style_image.read(), style_type='Custom')
+    #* Start Image Processing in the background
+    compute_thread = ProcessImage(new_image)
     compute_thread.start()
 
     db.session.add(new_image)
     db.session.commit()
-
     return pass_schema.jsonify(new_image)
-  return "Not allowed name", 200
+  return "Not allowed file", 200
+
+
+@app.route('/image/prestyle/<style>', methods=['POST'])
+def add_pre_style(style):  #* Check imput
+  if request.files['content_image'] is None: return "Missing content image", 200
+  content_image = request.files['content_image']
+  style_image = open(app.config['STYLE_FOLDER'] + style_paths[style],'rb')
+
+  if content_image and style_image and allowed_file(content_image.filename):# and allowed_file(style_image.filename):
+    new_image = Image(content_image=content_image.read(), style_image=style_image.read(), style_type=style)
+    #* Start Image Processing in the background
+    compute_thread = ProcessImage(new_image)
+    compute_thread.start()
+
+    db.session.add(new_image)
+    db.session.commit()
+    return pass_schema.jsonify(new_image)
+  return "Not allowed file", 200
 
 
 @app.route('/image/<id>', methods=['GET'])
@@ -107,9 +125,16 @@ def get_image(id):
         return "Image not ready", 200
       elif result_image.status == -1:
         return "There have been an error", 200
+      #? Delete the file after or not?
       return send_file(BytesIO(result_image.result_image), attachment_filename="test.png"), 200
     else:
       return "Wrong password", 200
+
+
+@app.route('/styles/', methods=['GET'])
+def get_styles():
+  styles = style_paths.keys()
+  return '[' + ', '.join(map(str, styles)) + ']', 200
 
 
 @app.route('/image/ids', methods=['GET'])
