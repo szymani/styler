d057d6b62e182c3a9650c2f008794f8f25c431d8
Szymani <Szymanski996@gmail.com>
2020-07-22 12:26:06 +0200

    updated marshmallow schemas

--- server_side/application/schemas/schemas.py
+++ server_side/application/schemas/schemas.py
@@ -9,39 +9,43 @@ class PostSchema(SQLAlchemyAutoSchema):
         model = SinglePost
         include_relationships = True
         load_instance = True
-
+    comments = ma.Nested("CommentSchema", many=True)
 
 class StyleSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Style
         include_relationships = True
         load_instance = True
-
+    style_author = ma.Nested("UserSchema", only=["login","profile_photo","id"], many=False)
+    fav_for = ma.Nested("UserSchema", only=["login","profile_photo","id"], many=True)
 
 class CommentSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Comment
         include_relationships = True
         load_instance = True
-
+    comment_author = ma.Nested("UserSchema", only=["login","profile_photo","id"], many=False)
 
 class MessageSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Message
         include_relationships = True
         load_instance = True
-
-
-class ChatSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Chat
-        include_relationships = True
-        load_instance = True
-    messages = ma.Nested(MessageSchema, many=True)
-
+    author = ma.Nested("UserSchema", only=["login","profile_photo","id"], many=False)
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
         include_relationships = True
         load_instance = True
+
+class ChatSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Chat
+        include_relationships = True
+        load_instance = True
+    messages = ma.Nested("MessageSchema", many=True)
+    participants = ma.Nested(UserSchema, only=["login","profile_photo","id"], many=True)
+
+
+
